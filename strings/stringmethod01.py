bef47466bd37267891cef3854630fc21e877e4b6
CodeDefenderNate <nathan.k.haley@gmail.com>
2024-03-26 15:52:03 +0000

    join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/ python3
+
+lilstring = "Altae3 Research offers classes on Python coding"
+newlist = lilstring.split(" ")
+print(newlist)
+
+myiplist = ["192", "168", "0", "12"]
+
+singleip = ".".join(myiplist)
+
+print(singleip)
---
+++ strings/stringmethod02.py
@@ -0,0 +1,16 @@
+#!/usr/bin python3
+
+def main():
+    """ Run-time code """
+
+    lilstring = "Alta3 Research offers classes on Python coding"
+    newlist = lilstring.split(" ")
+    print(newlist)
+
+    myiplist = ["192", "168", "0", "12"]
+
+    singleip = ".".join(myiplist)
+
+    print(singleip)
+
+main()
